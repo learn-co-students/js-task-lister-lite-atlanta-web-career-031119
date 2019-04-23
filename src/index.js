32e8932ff6bc6be1d5bb592fc410a7fdb2b84cde
Ansara Hollinger <ansara.hooks@gmail.com>
2019-04-23 15:46:45 -0400

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("create-task-form").addEventListener("submit", handleSubmit)
+})
+
+function handleSubmit(e){
+  e.preventDefault()
+  let form = e.target
+  const task = {new_task_description: form.querySelector("#new-task-description").value}
+  addNewTask(task)
+  e.target.reset()
+}
+
+function addNewTask(task){
+  const tasks = document.getElementById("tasks")
+  const li = document.createElement("li")
+  li.textContent = task.new_task_description
+  const deleteButton = document.createElement("button")
+  deleteButton.innerText = "X"
+  deleteButton.addEventListener("click", handleClick)
+  tasks.appendChild(li)
+  li.className = ""
+  li.appendChild(deleteButton)
+}
+
+function handleClick(e){
+  e.target.parentElement.remove()
+}
