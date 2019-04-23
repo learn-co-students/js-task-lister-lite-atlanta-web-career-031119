acbe7492d5aedb76626a30320beb817825078c1b
Samidh Patel <spatel1345@gmail.com>
2019-04-23 15:18:16 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    document.querySelector('#submit').addEventListener('click', handleClick)
+
+
 });
+
+function addToList(task) {
+
+  const ul = document.querySelector('#list')
+  const li = document.createElement('li')
+    li.className = 'todo'
+    li.textContent = task
+    
+    ul.appendChild(li)
+}
+
+function handleClick(e){
+  e.preventDefault()
+  console.log(e.target)
+  task = document.querySelector('#new-task-description').value
+  
+  
+  addToList(task)
+}
+
+
+
+
